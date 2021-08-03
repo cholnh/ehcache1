c2fec5504f330c149fb67686bba5acfb5f3bac0a
Nzzi <cholnh1@naver.com>
2021-08-03 18:44:30 +0900

    aop - self invocation 해결부 추가

--- src/main/java/kr/nzzi/test/ehcache1/service/impl/ProductServiceImpl.java
+++ src/main/java/kr/nzzi/test/ehcache1/service/impl/ProductServiceImpl.java
@@ -5,11 +5,15 @@ import kr.nzzi.test.ehcache1.service.ProductService;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Resource(name="productServiceImpl")
+    private ProductServiceImpl self;
 
     @Override
     @Cacheable(cacheNames = "testCache1")
@@ -18,16 +22,21 @@ public class ProductServiceImpl implements ProductService {
         return Product.of(id, "product"+id);
     }
 
+    /**
+     * self-invocation 발생
+     * @Cacheable 은 AOP 를 사용하므로 내부 호출로 인한 self-invocation 발생하여,
+     * findOne 메서드의 어드바이스가 실행되지 않음. 즉 Cacheable 이 미작동.
+     *
+     * self-invocation 해결
+     * 자기 자신을 빈으로 주입받아 프록시가 아닌 자신의 실체 메서드를 호출한다.
+     * self.findOne 은 프록시를 접근하지 않으므로 어드바이스가 잘 실행된다.
+     */
     @Override
-    @Cacheable(cacheNames = "testCache1") // self-invocation 발생...
+    @Cacheable(cacheNames = "testCache1")
     public Set<Product> findMulti(Set<Long> ids) {
-
         Set<Product> resultSet = new LinkedHashSet<>();
-
-        for (Long id : ids) {
-            resultSet.add(findOne(id));
-        }
-
+        for (Long id : ids)
+            resultSet.add(self.findOne(id));
         return resultSet;
     }
 
